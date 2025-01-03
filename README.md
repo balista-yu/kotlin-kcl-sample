cb5a092919006ca6abb0f9f671da32ac13bd8b08
y-uranaka <yuuykeek@gmail.com>
2025-01-03 14:35:41 +0900

    postgres周りをリファクタリング

--- README.md
+++ README.md
@@ -20,18 +20,7 @@ $ cd study-kotlin
 $ task up
 ```
 
-3. Create Table with PostgresSQL
-```sql
-CREATE TABLE kinesis_data (
-    id SERIAL PRIMARY KEY,
-    partition_key VARCHAR(256) NOT NULL,
-    sequence_number VARCHAR(256) NOT NULL,
-    data TEXT NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-```
-
-4. DynamoDB Insert Record with localstack
+3. DynamoDB Insert Record with localstack
 ```
 # awslocal dynamodb put-item --table-name study_kotlin --item '{"id": {"S": "hoge"}}'
 ```
--- infra/docker/postgres/init/init.sql
+++ infra/docker/postgres/init/init.sql
@@ -2,17 +2,37 @@ DO
 $$
 BEGIN
     IF
-EXISTS (SELECT FROM pg_database WHERE datname = 'kotlin2_test') THEN
-        PERFORM pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname = 'kotlin2_test';
+EXISTS (SELECT FROM pg_database WHERE datname = 'study_kotlin_test') THEN
+        PERFORM pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname = 'study_kotlin_test';
         DROP
-DATABASE kotlin2_test;
+DATABASE study_kotlin_test;
 END IF;
 END
 $$;
 
-CREATE DATABASE kotlin2_test
+CREATE DATABASE study_kotlin_test
     WITH OWNER = 'test'
     ENCODING = 'UTF8'
     LC_COLLATE = 'ja_JP.UTF-8'
     LC_CTYPE = 'ja_JP.UTF-8'
     TEMPLATE = template0;
+
+\c study_kotlin_test;
+
+CREATE TABLE IF NOT EXISTS kinesis_data (
+    id SERIAL PRIMARY KEY,
+    partition_key VARCHAR(256) NOT NULL,
+    sequence_number VARCHAR(256) NOT NULL,
+    data TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+\c study_kotlin;
+
+CREATE TABLE IF NOT EXISTS kinesis_data (
+    id SERIAL PRIMARY KEY,
+    partition_key VARCHAR(256) NOT NULL,
+    sequence_number VARCHAR(256) NOT NULL,
+    data TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
