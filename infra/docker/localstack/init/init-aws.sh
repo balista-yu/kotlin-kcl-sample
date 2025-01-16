5e5c03244c51c5a81cdc042d79b792541323268e
y-uranaka <yuuykeek@gmail.com>
2025-01-16 22:30:25 +0900

    マルチストリーム処理用にテーブルを増やす

--- infra/docker/localstack/init/init-aws.sh
+++ infra/docker/localstack/init/init-aws.sh
@@ -2,7 +2,17 @@
 
 # create dynamodb
 awslocal dynamodb create-table \
-    --table-name study_kotlin \
+    --table-name animals \
+    --attribute-definitions \
+        AttributeName=id,AttributeType=S \
+    --key-schema \
+        AttributeName=id,KeyType=HASH \
+    --billing-mode=PAY_PER_REQUEST \
+    --no-deletion-protection-enabled \
+    --stream-specification StreamEnabled=true,StreamViewType=NEW_IMAGE
+
+awslocal dynamodb create-table \
+    --table-name foods \
     --attribute-definitions \
         AttributeName=id,AttributeType=S \
     --key-schema \
@@ -13,7 +23,17 @@ awslocal dynamodb create-table \
 
 # create dynamodb test用
 awslocal dynamodb create-table \
-    --table-name study_kotlin_test \
+    --table-name animals_test \
+    --attribute-definitions \
+        AttributeName=id,AttributeType=S \
+    --key-schema \
+        AttributeName=id,KeyType=HASH \
+    --billing-mode=PAY_PER_REQUEST \
+    --no-deletion-protection-enabled \
+    --stream-specification StreamEnabled=true,StreamViewType=NEW_IMAGE
+
+awslocal dynamodb create-table \
+    --table-name foods_test \
     --attribute-definitions \
         AttributeName=id,AttributeType=S \
     --key-schema \
@@ -24,22 +44,40 @@ awslocal dynamodb create-table \
 
 # create kinesis stream
 awslocal kinesis create-stream \
-    --stream-name study-kotlin-stream \
-    --shard-count 1
+    --stream-name animals-stream \
+    --shard-count 1 \
+    --stream-mode-details '{"StreamMode": "ON_DEMAND"}'
+
+awslocal kinesis create-stream \
+    --stream-name foods-stream \
+    --shard-count 1 \
+    --stream-mode-details '{"StreamMode": "ON_DEMAND"}'
 
 # create kinesis stream test用
 awslocal kinesis create-stream \
-    --stream-name study-kotlin-stream-test \
-    --shard-count 1
+    --stream-name animals-stream-test \
+    --shard-count 1 \
+    --stream-mode-details '{"StreamMode": "ON_DEMAND"}'
+
+awslocal kinesis create-stream \
+    --stream-name foods-stream-test \
+    --shard-count 1 \
+    --stream-mode-details '{"StreamMode": "ON_DEMAND"}'
 
 # activate dynamodb kinesis data stream
 awslocal dynamodb enable-kinesis-streaming-destination \
-    --table-name study_kotlin \
-    --stream-arn arn:aws:kinesis:ap-northeast-1:000000000000:stream/study-kotlin-stream \
-    --enable-kinesis-streaming-configuration ApproximateCreationDateTimePrecision=MICROSECOND
+    --table-name animals \
+    --stream-arn arn:aws:kinesis:ap-northeast-1:000000000000:stream/animals-stream
+
+awslocal dynamodb enable-kinesis-streaming-destination \
+    --table-name foods \
+    --stream-arn arn:aws:kinesis:ap-northeast-1:000000000000:stream/foods-stream
 
 # activate dynamodb kinesis data stream test用
 awslocal dynamodb enable-kinesis-streaming-destination \
-    --table-name study_kotlin_test \
-    --stream-arn arn:aws:kinesis:ap-northeast-1:000000000000:stream/study-kotlin-stream-test \
-    --enable-kinesis-streaming-configuration ApproximateCreationDateTimePrecision=MICROSECOND
+    --table-name animals_test \
+    --stream-arn arn:aws:kinesis:ap-northeast-1:000000000000:stream/animals-stream-test
+
+awslocal dynamodb enable-kinesis-streaming-destination \
+    --table-name foods_test \
+    --stream-arn arn:aws:kinesis:ap-northeast-1:000000000000:stream/foods-stream-test
